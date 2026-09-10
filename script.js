6f144ebc1db80ac662dc89a5552ec1c426f4bc22
Wollin03 <frederikke@iversen-mail.dk>
2026-09-10 12:40:15 +0200

    10/9

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heroVideo = document.getElementById("hero-video");
+    if (heroVideo) {
+        heroVideo.playbackRate =0.60;
+}
+});
+
