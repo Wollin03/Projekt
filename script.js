6846e76433fe46d012eebdad096f0f555b5f948f
Wollin03 <frederikke@iversen-mail.dk>
2026-09-17 08:58:52 +0200

    17/9

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 }
 });
 
+const farvePrikker = document.querySelectorAll('.farve-prik');
+const farveHotspots = document.querySelectorAll('.farve-hotspot');
+
+farvePrikker.forEach(prik => {
+  prik.addEventListener('click', () => {
+    const valgtFarve = prik.dataset.farve;
+
+    farvePrikker.forEach(p => p.classList.remove('aktiv'));
+    farveHotspots.forEach(h => h.classList.remove('aktiv'));
+
+    prik.classList.add('aktiv');
+    document.querySelector(`.farve-hotspot-${valgtFarve}`).classList.add('aktiv');
+  });
+});
